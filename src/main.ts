865487dd7370324ed27c98c6282df7b7b1968696
florian.toribio <florian.toribio@odandb.com>
2024-06-07 11:42:59 +0200

    add utils

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import "/src/styles/reset.css";
 import "/src/styles/style.scss";
 
+let gameRepository: GameRepository;
+
 window.addEventListener("load", function () {
     console.log("Hello World!");
+    init();
 });
+
+function init() {
+    const map = new GameMap(10, 10);
+    gameRepository = new GameRepository(0, map);
+}
